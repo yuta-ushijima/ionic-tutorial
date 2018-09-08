49c517c831aff813c2e8533f4a947cb692b4b791
yuta-ushijima <register@yuta-u.com>
2018-09-09 00:16:38 +0900

    タスクの変更/削除をAPIと組み合わせて実装する

--- src/pages/task-list/task-list.ts
+++ src/pages/task-list/task-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the TaskListPage page.
@@ -19,8 +19,12 @@ export class TaskListPage {
     {name: 'タスク2'},
   ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  //クラス内で使用できるように、ActionSheetControllerをactionSheetCtrlの引数に注入
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public actionSheetCtrl: ActionSheetController,
+              public alertCtrl: AlertController
+  ) {}
 
   ionViewWillEnter() {
     if(localStorage.getItem('tasks')){
@@ -32,4 +36,62 @@ export class TaskListPage {
     console.log('ionViewDidLoad TaskListPage');
   }
 
+  changeTask(index: number) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'タスクの変更',
+      buttons: [
+        {
+          text: '削除',
+          role: 'destructive',
+          //handler内の処理は、削除をクリックした時の挙動
+          handler: () => {
+            //tasksのindex番目を削除する処理を書く
+            this.tasks.splice(index, 1);
+            // localStorageの値を上書き保存
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
+          }
+        },{
+          text: '変更',
+          handler: () => {
+            this._renameTask(index);
+          }
+        },{
+          text: '閉じる',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked')
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  _renameTask(index: number){
+    let prompt = this.alertCtrl.create({
+      title: '変更後のタスク',
+      inputs: [
+        {
+          name: 'task',
+          placeholder: 'タスク',
+          value: this.tasks[index].name
+        },
+      ],
+      buttons: [
+        {
+          text: '閉じる'
+        },
+        {
+          text: '保存',
+          handler: data => {
+          //  タスクのindex番目を上書き
+            this.tasks[index] = { name: data.task };
+          //  localStorageに保存
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
