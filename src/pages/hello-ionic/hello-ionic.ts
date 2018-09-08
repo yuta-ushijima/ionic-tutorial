6ce8cdafe8b1c31c8e2bb56d5420c87bd32ef3a2
yuta-ushijima <register@yuta-u.com>
2018-09-08 23:01:31 +0900

    HTMLに直接書いていない値を表示する

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
+  // string型のtitle変数を定義し、デフォルトの値を代入する
+  title: string = 'タスク登録';
+  //　配列tasksを用意し、オブジェクトにstring型のnameを格納
+  //  nameをキーとしたオブジェクトの型が{ name: string } でそれを配列[]で格納
+  // 配列[]はArray<>でも表記可能
+  tasks: { name: string }[] = [
+    { name: 'タスク1' },
+    { name: 'タスク2' },
+  ];
   constructor() {
 
   }
