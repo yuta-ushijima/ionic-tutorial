71aef6ba72bc56eee6597a73c1323f5a52fef2d5
yuta-ushijima <register@yuta-u.com>
2018-09-08 23:25:18 +0900

    タスク一覧を別ページで作成する

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -14,7 +14,32 @@ export class HelloIonicPage {
     { name: 'タスク1' },
     { name: 'タスク2' },
   ];
+  //プロパティtaskを定義(テンプレート側で[(ngModel)]にて双方向データバインディングを行うため)
+  task: string;
   constructor() {
 
+  }
+
+  ionViewWillEnter() {
+    //localStrageにtasksが保存されている場合はtasksを取り出す
+    if(localStorage.getItem('tasks')){
+      // JSON.parse()を使って、JSON文字列のtasksを配列に戻し、プロパティtasksを上書き
+      this.tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+  }
+
+  addTask() {
+    /* プロパティにメソッド内からアクセスするためにthisを使う
+       配列の末尾にpushで要素を追記
+    */
+    this.tasks.push({
+      name: this.task
+    });
+    /* localStorageでデータの永続化ができるのはstring型だけ。
+    * そのため、配列を格納する場合は、下記のようにJSON形式で変換する
+    * この場合、第一引数のkeyが保存する名前になる。*/
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    /* pushが終わったら、初期化のために<ion-input>を空にする */
+    this.task = '';
   }
 }
